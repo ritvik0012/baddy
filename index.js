43564206ae5aaa4bfb8aebd95a64484eb88ba885
ritvik0012 <ritvik0012@gmail.com>
2024-01-07 23:38:05 +0530

    created a socket server

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const bodyParser = require('body-parser');
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render("")
 })
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log("A new user connected");
+})
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
