7bf1fa0b1a699e4e982136c4d288e432e5b99e33
ritvik0012 <ritvik0012@gmail.com>
2024-01-09 21:54:33 +0530

    added models

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const bodyParser = require('body-parser');
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const {MongoClient, ServerApiVersion} = require("mongodb");
+const mongoose = require('mongoose');
+const User = require('./models/users');
 const io = new Server(server);
+
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -15,8 +19,27 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const uri = "mongodb://localhost:27017/baddy";
+
+mongoose.connect(uri);
+
+
 app.get('/', (req, res) => {
   res.render("")
+})
+
+app.post('/login', (req,res) => {
+  console.log(req.body)
+  const user = new User({
+    username: req.body.uname,
+    password: req.body.psw,
+  });
+
+  user.save().then((value) => {
+    console.log(value)
+    console.log("Entry added in database");
+  }).catch((err) => console.log(err));
+  
 })
 
 io.on('connection', (socket) => {
